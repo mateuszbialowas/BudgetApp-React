59be0f819c1dd2285012a475ca247b23277bec15
MateuszBialowas <mateusz.bialowas2@gmail.com>
2021-12-15 18:58:58 +0100

    hook Budget

--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../contex/AppContext";
 
 export default function Budget() {
+  const { budget } = useContext(AppContext);
   return (
     <div
       className="bg-blue-100 rounded-lg p-6 text-sm text-blue-700 dark:bg-blue-200 dark:text-blue-800"
       role="alert"
-    >Budget: $2000
+    >
+      Budget: {`$${budget}`}
     </div>
   );
 }

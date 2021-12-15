21a19c2fc7a5e2a4bab4abd448df0ef1783d3260
MateuszBialowas <mateusz.bialowas2@gmail.com>
2021-12-15 18:52:18 +0100

    create the context

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import Remaining from "./components/Remaining";
 import ExpenseTotal from "./components/ExpenseTotal";
 import ExpenseList from "./components/ExpenseList";
 import AddExpenseForm from "./components/AddExpenseForm";
+import { AppProvider } from "./contex/AppContext";
 
 function App() {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="font-medium text-5xl md:text-7xl text-center">Budget App</h1>
-      <div className="mt-4 grid grid-cols-1 lg:grid-cols-3 gap-4">
-        <Budget />
-        <Remaining />
-        <ExpenseTotal />
+    <AppProvider>
+      <div className="container mx-auto p-4">
+        <h1 className="font-medium text-5xl md:text-7xl text-center">
+          Budget App
+        </h1>
+        <div className="mt-4 grid grid-cols-1 lg:grid-cols-3 gap-4">
+          <Budget />
+          <Remaining />
+          <ExpenseTotal />
+        </div>
+        <ExpenseList />
+        <AddExpenseForm />
       </div>
-      <ExpenseList/>
-      <AddExpenseForm/>
-    </div>
+    </AppProvider>
   );
 }
 

a92b3e93560f98000e94f65f454be524fd490521
MateuszBialowas <mateusz.bialowas2@gmail.com>
2021-12-16 01:24:36 +0100

    add new expense

--- src/components/AddExpenseForm.jsx
+++ src/components/AddExpenseForm.jsx
@@ -1,11 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { AppContext } from "../contex/AppContext";
+import { v4 as uuidv4 } from "uuid";
 
 export default function AddExpenseForm() {
+  const { dispatch } = useContext(AppContext);
   const [name, setName] = useState("");
   const [cost, setCost] = useState("");
 
   const onSubmit = (event) => {
     event.preventDefault();
+
+    const expense = {
+      id: uuidv4(),
+      name: name,
+      cost: parseInt(cost),
+    };
+
+    dispatch({
+      type: 'ADD_EXPENSE',
+      payload: expense
+    })
   };
   return (
     <div>

a3a3731eaeca002a2911d8d4705e11613d5a68f8
MateuszBialowas <mateusz.bialowas2@gmail.com>
2021-12-16 01:48:30 +0100

    expenseTotal component

--- src/components/ExpenseTotal.jsx
+++ src/components/ExpenseTotal.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../contex/AppContext";
 
 export default function ExpenseTotal() {
+  const { expenses } = useContext(AppContext);
+
+  const totalExpences = expenses.reduce((total, item) => total + item.cost, 0);
   return (
     <div
       className="bg-yellow-100 rounded-lg p-6 text-sm text-yellow-700 dark:bg-yellow-200 dark:text-yellow-800"
       role="alert"
     >
-      Spent so far: $1500
+      Spent so far: {`$${totalExpences}`}
     </div>
   );
 }

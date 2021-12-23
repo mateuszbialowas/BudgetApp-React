176a7fc8557c78d412196a4ec1c5faee79d39a42
MateuszBialowas <mateusz.bialowas2@gmail.com>
2021-12-23 23:41:42 +0100

    Add toast on fetching budget

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -37,19 +37,39 @@ const initialState = {
 
 export const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
-  let { user } = useUserAuth();
+  const { user } = useUserAuth();
 
   useEffect(() => {
     if (user) {
+      const fetchBudgetToast = toast.loading("Fetching budget...");
+      console.log("Budget fetching...");
       const dbRef = ref(database);
       get(child(dbRef, "users/" + user.uid + "/budget"))
         .then((snapshot) => {
           if (snapshot.exists()) {
             dispatch({ type: "SET_BUDGET", payload: snapshot.val() });
+            toast.update(fetchBudgetToast, {
+              render: "Budget fetched!",
+              type: "success",
+              isLoading: false,
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnFocusLoss: false,
+            });
           } else {
             set(child(dbRef, "users/" + user.uid + "/budget"), 1500)
               .then(() => {
                 dispatch({ type: "SET_BUDGET", payload: 1500 });
+                toast.update(fetchBudgetToast, {
+                  render: "Set default budget to $1500",
+                  type: "success",
+                  isLoading: false,
+                  autoClose: 5000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnFocusLoss: false,
+                });
               })
               .catch((error) => {
                 console.log(
@@ -59,13 +79,19 @@ export const AppProvider = ({ children }) => {
           }
         })
         .catch((error) => {
-          console.log(
-            "AppProvider: useEffect: GET_BUDGET: Error: " + error
-          );
+          console.log("AppProvider: useEffect: GET_BUDGET: Error: " + error);
+          toast.update(fetchBudgetToast, {
+            render: "Error fetching budget!",
+            type: "error",
+            isLoading: false,
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnFocusLoss: false,
+          });
         });
     }
   }, [user]);
-
   return (
     <AppContext.Provider
       value={{

07015d4547773c121172b4ccac0639fffba13870
MateuszBialowas <mateusz.bialowas2@gmail.com>
2022-01-15 14:55:35 +0100

    Edit category.

--- src/components/budgetApp/EditExpenseForm.jsx
+++ src/components/budgetApp/EditExpenseForm.jsx
@@ -18,7 +18,7 @@ export default function EditExpenseForm(props) {
       cost: parseFloat(cost),
       details: details,
       date: date,
-      category: category,
+      category: form.category.value,
       categoryEmoji: categoryEmoji,
     };
 
@@ -46,6 +46,22 @@ export default function EditExpenseForm(props) {
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               placeholder={name}
               defaultValue={name}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="category"
+              className="block mb-2 text-sm font-medium text-gray-900"
+            >
+              Category
+            </label>
+            <input
+              type="text"
+              id="category"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeholder={category}
+              defaultValue={category}
               required
             />
           </div>

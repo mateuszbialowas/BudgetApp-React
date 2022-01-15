f8e1145419543510c8be13f211f63f885e5eb4fa
MateuszBialowas <mateusz.bialowas2@gmail.com>
2022-01-15 13:47:00 +0100

    Added details and date.

--- src/components/budgetApp/AddExpenseForm.jsx
+++ src/components/budgetApp/AddExpenseForm.jsx
@@ -93,6 +93,38 @@ export default function AddExpenseForm() {
         </div>
         <div className="mb-6">
           <label
+            htmlFor="details"
+            className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+          >
+            Details
+          </label>
+          <input
+            type="text"
+            id="details"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+            value={details}
+            onChange={(event) => setDetails(event.target.value)}
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="date"
+            className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+          >
+            Date
+          </label>
+          <input
+            type="date"
+            id="date"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+            value={date}
+            onChange={(event) => setDate(event.target.value)}
+          />
+        </div>
+        <div className="mb-6">
+          <label
             htmlFor="cost"
             className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
           >
